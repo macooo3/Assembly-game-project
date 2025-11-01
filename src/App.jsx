019b0cd56ad119fc6e75e77147fe9fc67b36da82
macooo3 <marcoysu765@gmail.com>
2025-10-31 22:20:58 -0400

    created the language cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import StatusPopup from "./components/StatusPopup";
+import { languages } from "./assets/languages";
 
 function App() {
   return (
     <main>
       <Header />
       <StatusPopup />
+      <section className="languages">
+        {languages.map((lang) => (
+          <div
+            key={lang.name}
+            className="language-card"
+            style={{
+              backgroundColor: lang.backgroundColor,
+              color: lang.color,
+            }}
+          >
+            {lang.name}
+          </div>
+        ))}
+      </section>
     </main>
   );
 }
