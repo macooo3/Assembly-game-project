822e6ce848bf10f160f1bde5d73578a97d023781
macooo3 <marcoysu765@gmail.com>
2025-11-02 23:47:04 -0500

    created keyboard layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,48 @@ import StatusPopup from "./components/StatusPopup";
 import { languages } from "./assets/languages";
 
 function App() {
+  const [currentWord, setCurrentWord] = useState("react");
+
+  const alphabet = "abcdefghijklmopqrstuvwxyz";
+
+  const languageElements = languages.map((lang) => (
+    <div
+      key={lang.name}
+      className="language-card"
+      style={{
+        backgroundColor: lang.backgroundColor,
+        color: lang.color,
+      }}
+    >
+      {lang.name}
+    </div>
+  ));
+
+  const guessWord = currentWord
+    .toUpperCase()
+    .split("")
+    .map((word, index) => (
+      <span key={index} className="word-card">
+        {word}
+      </span>
+    ));
+
+  const keyboardBtn = alphabet
+    .toUpperCase()
+    .split("")
+    .map((letters, index) => (
+      <button key={index} className="letterBtn">
+        {letters}
+      </button>
+    ));
+
   return (
     <main>
       <Header />
       <StatusPopup />
-      <section className="languages">
-        {languages.map((lang) => (
-          <div
-            key={lang.name}
-            className="language-card"
-            style={{
-              backgroundColor: lang.backgroundColor,
-              color: lang.color,
-            }}
-          >
-            {lang.name}
-          </div>
-        ))}
-      </section>
+      <section className="languages">{languageElements}</section>
+      <section className="word">{guessWord}</section>
+      <section className="keyboard">{keyboardBtn}</section>
     </main>
   );
 }
